9010708b43ae2203350d1bd92b5d4c1ab91be8d6
ciembor <pub@ciemborowicz.pl>
2013-09-22 22:56:58 +0200

    TimeWatcher onTime.

--- src/watchers/TimeWatcher.js
+++ src/watchers/TimeWatcher.js
@@ -18,6 +18,22 @@ TimeWatcher.prototype = {
 		setInterval(function() {
 			onEvent(actions);
 		}, seconds * 1000);
+	},
+	onTime: function(time, actions) {
+		var time = time.split(':');
+		var time_in_sec = 0;
+		var current_time = new Date();
+		var current_time_in_sec = current_time.getSeconds() + (60 * current_time.getMinutes()) + (60 * 60 * current_time.getHours());
+		for (i=0; i<3; ++i) {
+			if (time[2-i]) {
+				time_in_sec += Math.pow(time[2-i], 60);
+			}
+		}
+		setTimeout(function() {
+			setInterval(function() {
+				onEvent(actions)	;
+			}, 24 * 60 * 60 * 1000);
+		}, time_in_sec + Math.abs(current_time_in_sec - 24 * 60 * 60) * 1000);
 	}
 };
 
