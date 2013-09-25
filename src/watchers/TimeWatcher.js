c30da767ac3bb8098502112a3c3b8d46f743b169
Maciej Ciemborowicz <maciej.ciemborowicz@llp.pl>
2013-09-25 18:38:44 +0200

    Fix for TimeWatcher.js

--- src/watchers/TimeWatcher.js
+++ src/watchers/TimeWatcher.js
@@ -26,15 +26,22 @@ TimeWatcher.prototype = {
 		var current_time_in_sec = current_time.getSeconds() + (60 * current_time.getMinutes()) + (60 * 60 * current_time.getHours());
 		for (i=0; i<3; ++i) {
 			if (time[2-i]) {
-				time_in_sec += Math.pow(time[2-i], 60);
+				time_in_sec += time[2-i] * Math.pow(60, i);
 			}
 		}
-		setTimeout(function() {
+    var delay_in_sec = 0;
+    if (time_in_sec > current_time_in_sec) {
+      delay_in_sec = time_in_sec - current_time_in_sec;
+    } else {
+      delay_in_sec = time_in_sec + 60 * 60 * 24 - current_time_in_sec;
+    }
+    setTimeout(function() {
+      onEvent(actions);
 			setInterval(function() {
-				onEvent(actions)	;
+				onEvent(actions);
 			}, 24 * 60 * 60 * 1000);
-		}, time_in_sec + Math.abs(current_time_in_sec - 24 * 60 * 60) * 1000);
-	}
+		}, delay_in_sec * 1000);
+  }
 };
 
 module.exports = TimeWatcher;
